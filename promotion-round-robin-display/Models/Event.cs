d76a74809e032f2db14aa43a3b854b630748b123
Ponjira (Auy) Laping <pla@manaosoftware.com>
2025-07-11 13:41:03 +0700

    - add Event, Promotion models and main function

---
+++ promotion-round-robin-display/Models/Event.cs
@@ -0,0 +1,13 @@
+﻿namespace promotion_round_robin_display.Models
+{
+    public class Event
+    {
+        public int Score { get; set; }
+        public List <string> Promotions { get; set; }
+        public Event(int score, List<string> promotions)
+        {
+            Score = score;
+            Promotions = promotions;
+        }
+    }
+}
---
+++ promotion-round-robin-display/Models/Promotion.cs
@@ -0,0 +1,13 @@
+﻿namespace promotion_round_robin_display.Models
+{
+    public class Promotion
+    {
+        public string Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public Promotion(string id, DateTime startDate)
+        {
+            Id = id;
+            StartDate = startDate;
+        }
+    }
+}
