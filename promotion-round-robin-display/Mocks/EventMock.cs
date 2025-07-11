918f58b4024a8d1ecafc5b2987c7d601e77d9a3c
Ponjira (Auy) Laping <pla@manaosoftware.com>
2025-07-11 14:15:05 +0700

    - add Mock data of events and Promotion

---
+++ promotion-round-robin-display/Mocks/EventMock.cs
@@ -0,0 +1,20 @@
+﻿using promotion_round_robin_display.Models;
+using promotion_round_robin_display.Mocks; // Add this to access PromotionMock
+
+namespace promotion_round_robin_display.Mocks
+{
+    public class EventMock
+    {
+        public EventMock() { }
+
+        public static List<Event> GetEvents()
+        {
+            return new List<Event>
+            {
+                new Event(7, PromotionMock.GetPromotionsA_C()),
+                new Event(6, PromotionMock.GetPromotionsD_F()),
+                new Event(5, PromotionMock.GetPromotionsG_I())
+            };
+        }
+    }
+}
---
+++ promotion-round-robin-display/Mocks/PromotionMock.cs
@@ -0,0 +1,41 @@
+﻿using promotion_round_robin_display.Models;
+
+namespace promotion_round_robin_display.Mocks
+{
+    public class PromotionMock
+    {
+        public PromotionMock()
+        {
+        }
+
+        public static List<Promotion> GetPromotionsA_C()
+        {
+            return new List<Promotion>
+            {
+                new Promotion("a", DateTime.Parse("2024-09-01")),
+                new Promotion("b", DateTime.Parse("2024-09-05")),
+                new Promotion("c", DateTime.Parse("2024-09-10")),
+            };
+        }
+        public static List<Promotion> GetPromotionsD_F()
+        {
+            return new List<Promotion>
+            {
+                new Promotion("d", DateTime.Parse("2024-08-30")),
+                new Promotion("e", DateTime.Parse("2024-09-04")),
+                new Promotion("f", DateTime.Parse("2024-09-09")),
+            };
+        }
+
+        public static List<Promotion> GetPromotionsG_I()
+        {
+            return new List<Promotion>
+            {
+                new Promotion("g", DateTime.Parse("2024-09-02")),
+                new Promotion("h", DateTime.Parse("2024-09-06")),
+                new Promotion("i", DateTime.Parse("2024-09-11")),
+            };
+        }
+
+    }
+}
